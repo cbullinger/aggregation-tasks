4962539b523972afebaee278ba397615037a892d
cbullinger <cory.bullinger@mongodb.com>
2025-06-05 15:16:18 -0400

    update readme

--- python/config.py
+++ python/config.py
@@ -3,9 +3,9 @@ import os
 
 load_dotenv()
 
-# TODO Set connection string
-uri = "PASTE-CONNECTION-STRING-HERE"
+uri = "mongodb+srv://pymongo:SMa3tCqx3HNAhjEc@cluster0.ztsso0w.mongodb.net"
 
+# Set connection string
 MONGODB_URI = os.getenv("MONGODB_URI", uri)
 if not MONGODB_URI:
     raise RuntimeError("MONGODB_URI must be set in .env")
