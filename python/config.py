4d494cc2c66d31f473afc4f7cf3320843ef74e72
Cory <115956901+cbullinger@users.noreply.github.com>
2025-06-06 07:33:56 -0400

    Update config.py

--- python/config.py
+++ python/config.py
@@ -1,13 +1,2 @@
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
-# TODO Set connection string
-uri = "PASTE-CONNECTION-STRING-HERE"
-
-MONGODB_URI = os.getenv("MONGODB_URI", uri)
-if not MONGODB_URI:
-    raise RuntimeError("MONGODB_URI must be set in .env")
-
-DATABASE_NAME = os.getenv("DATABASE_NAME", "yelp")
+# Replace with MongoDB Atlas connection string provided by moderator
+MONGODB_URI = "mongodb+srv://username:password@clusteraddress.mongodb.net"
