6249fd4fdd29525c5396891d2195c88edf9a8b41
cbullinger <cory.bullinger@mongodb.com>
2025-10-26 13:57:00 -0400

    test deprecation file

--- java/src/main/java/com/example/aggregation/newsubdir/newtestfile.java
+++
@@ -1,7 +0,0 @@
-package com.example.aggregation.newsubdir;
-
-public class newtestfile {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
--- nodejs/src/tasks/newsubdir/testnode.js
+++
@@ -1,1 +0,0 @@
-This is a test file in the Node.js project.
